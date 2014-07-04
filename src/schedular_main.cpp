f5767db63346df8ef7542f2fdcad7d4e98a113e0
simakmandeep@gmail.com <meghasimak@gmail.com>
2014-07-04 23:29:53 +0530

    main file

---
+++ src/schedular_main.cpp
@@ -0,0 +1,29 @@
+/**
+ *       \file       schedular_main.cpp
+ *
+ *       \brief      Main function for calling Schedular
+ *
+ *       \compiler   g++\n
+ *
+ *       \author     Mandeep Kaur, simakmandeep@gmail.com
+ *       \license    GNU General Public License Version 2\n
+ *       \copyright  Copyright (c) 2014, GreatDevelopers\n
+ *                   https://github.com/GreatDevelopers
+ */
+
+#include "header/schedular.h"
+
+int main(void)
+{
+    Schedular rms;
+    rms.Main();
+
+    return 0;
+}
+
+/*
+ * Local Variables:
+ * tab-width: 4
+ * expandtab
+ * ex: shiftwidth=4 tabstop=4
+ */ 
